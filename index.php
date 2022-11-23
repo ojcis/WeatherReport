beda666ac5e1d963220c95fb07477212d4c00459
Ojārs <ojars.meldris@gmail.com>
2022-11-23 15:55:26 +0200

    update

--- index.php
+++ index.php
@@ -1,46 +1,19 @@
 <?php
 
 require_once 'vendor/autoload.php';
-require_once 'app/functions.php';
-require_once 'app/apiKey.php';
 
-use App\{WeatherData, AllForecastData};
-use function App\{
-    fetchCurrentWeather,
-    displayWeatherCurrentReport,
-    fetchWeatherForecast,
-    displayWeatherForecastReport
-};
+use App\ApiClient;
 
-echo "Weather API".PHP_EOL;
-echo "1. Display current weather".PHP_EOL;
-echo "2. Display weather forecast".PHP_EOL;
-echo "0. Exit".PHP_EOL;
-$selection = (int)readline("Input number (0-2): ");
+const API_KEY='cf60e647aafa33c963a3f5a489d1964b';
 
-switch ($selection) {
-    case 1:
-        $city = readline("Input location: ");
-        $weatherData = fetchCurrentWeather($city);
-        if (isset($weatherData)) {
-            $weatherData = new WeatherData($weatherData);
-            displayWeatherCurrentReport($weatherData);
-        } else {
-            echo "Couldn't gather the weather data!".PHP_EOL;
-        }
-        break;
-    case 2:
-        $city = readline("Input location: ");
-        do {
-            $days = (int)readline("Input forecast length in days (1-5): ");
-        } while ($days < 1 || $days > 5);
-        $weatherForecastData = fetchWeatherForecast($city, $days);
-        if (isset($weatherForecastData)) {
-            $weatherForecastData = new AllForecastData($weatherForecastData);
-            displayWeatherForecastReport($weatherForecastData, $days);
-        } else {
-            echo "Couldn't gather the weather data!".PHP_EOL;
-        }
-        break;
-    default:
-}
+$apiClient=new ApiClient(API_KEY);
+
+$location=readline('enter location: ');
+
+$weather=$apiClient->getWeather($location);
+
+echo "Weather in {$weather->getLocationName()}:".PHP_EOL;
+echo "temperature: {$weather->getTemperature()}°C, but feels like {$weather->getTemperatureFeelsLike()}°C".PHP_EOL;
+echo "humidity: {$weather->getHumidity()} %".PHP_EOL;
+echo "wind speed: {$weather->getWindSpeed()} m/s".PHP_EOL;
+
